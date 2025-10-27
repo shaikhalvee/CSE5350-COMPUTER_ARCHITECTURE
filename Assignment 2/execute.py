b8548c9f3ee49969d5c9e1a97d3c83c462cbd83c
Shaikh Islam <shaikhalvee@gmail.com>
2025-10-26 22:29:26 -0500

    error removed according to python 3

--- Assignment 2/execute.py
+++ Assignment 2/execute.py
@@ -21,7 +21,7 @@ dataseg = numregs - 2                                # next to last reg is a dat
 #ints and traps
 trapreglink = numregs - 3                            # store return value here
 trapval     = numregs - 4                            # pass which trap/int
-mem = [0] * realmemsize                              # this is memory, init to 0 
+mem = [0] * realmemsize                              # this is memory, init to 0
 reg = [0] * numregs                                  # registers
 clock = 1                                            # clock starts ticking
 ic = 0                                               # instruction count
@@ -31,17 +31,17 @@ starttime = time.time()
 curtime = starttime
 def startexechere ( p ):
     # start execution at this address
-    reg[ codeseg ] = p    
+    reg[ codeseg ] = p
 def loadmem():                                       # get binary load image
-  curaddr = 0
-  for line in open("a.out", 'r').readlines():
-    token = string.split( string.lower( line ))      # first token on each line is mem word, ignore rest
-    if ( token[ 0 ] == 'go' ):
-        startexechere(  int( token[ 1 ] ) )
-    else:    
-        mem[ curaddr ] = int( token[ 0 ], 0 )                
-        curaddr = curaddr = curaddr + 1
-def getcodemem ( a ):
+    curaddr = 0
+    for line in open("a.out", 'r').readlines():
+        token = line.lower().split()      # first token on each line is mem word, ignore rest
+        if token[ 0] == 'go':
+            startexechere(  int( token[ 1 ] ) )
+        else:
+            mem[ curaddr ] = int( token[ 0 ], 0 )
+            curaddr = curaddr = curaddr + 1
+def getcodemem(a):
     # get code memory at this address
     memval = mem[ a + reg[ codeseg ] ]
     return ( memval )
@@ -51,29 +51,29 @@ def getdatamem ( a ):
     return ( memval )
 def getregval ( r ):
     # get reg or indirect value
-    if ( (r & (1<<numregbits)) == 0 ):               # not indirect
-       rval = reg[ r ] 
+    if (r & (1 << numregbits)) == 0:               # not indirect
+       rval = reg[r]
     else:
        rval = getdatamem( reg[ r - numregs ] )       # indirect data with mem address
-    return ( rval )
+    return rval
 def checkres( v1, v2, res):
     v1sign = ( v1 >> (wordsize - 1) ) & 1
     v2sign = ( v2 >> (wordsize - 1) ) & 1
     ressign = ( res >> (wordsize - 1) ) & 1
-    if ( ( v1sign ) & ( v2sign ) & ( not ressign ) ):
-      return ( 1 )
-    elif ( ( not v1sign ) & ( not v2sign ) & ( ressign ) ):
-      return ( 1 )
+    if v1sign & v2sign & ( not ressign):
+      return 1
+    elif ( not v1sign) & ( not v2sign) & ( ressign):
+      return 1
     else:
-      return( 0 )
-def dumpstate ( d ):
-    if ( d == 1 ):
-        print reg
-    elif ( d == 2 ):
-        print mem
-    elif ( d == 3 ):
-        print 'clock=', clock, 'IC=', ic, 'Coderefs=', numcoderefs,'Datarefs=', numdatarefs, 'Start Time=', starttime, 'Currently=', time.time() 
-def trap ( t ):
+      return 0
+def dumpstate( d ):
+    if d == 1:
+        print(reg)
+    elif d == 2:
+        print(mem)
+    elif d == 3:
+        print('clock=', clock, 'IC=', ic, 'Coderefs=', numcoderefs,'Datarefs=', numdatarefs, 'Start Time=', starttime, 'Currently=', time.time())
+def trap (t, a=None):
     # unusual cases
     # trap 0 illegal instruction
     # trap 1 arithmetic overflow
@@ -81,18 +81,18 @@ def trap ( t ):
     # trap 3+ user
     rl = trapreglink                            # store return value here
     rv = trapval
-    if ( ( t == 0 ) | ( t == 1 ) ):
+    if (t == 0) | (t == 1):
        dumpstate( 1 )
        dumpstate( 2 )
        dumpstate( 3 )
-    elif ( t == 2 ):                          # sys call, reg trapval has a parameter
-       what = reg[ trapval ] 
-       if ( what == 1 ):
+    elif t == 2:                          # sys call, reg trapval has a parameter
+       what = reg[ trapval ]
+       if what == 1:
            a = a        #elapsed time
     return ( -1, -1 )
     return ( rv, rl )
-# opcode type (1 reg, 2 reg, reg+addr, immed), mnemonic  
-opcodes = { 1: (2, 'add'), 2: ( 2, 'sub'), 
+# opcode type (1 reg, 2 reg, reg+addr, immed), mnemonic
+opcodes = { 1: (2, 'add'), 2: ( 2, 'sub'),
             3: (1, 'dec'), 4: ( 1, 'inc' ),
             7: (3, 'ld'),  8: (3, 'st'), 9: (3, 'ldi'),
            12: (3, 'bnz'), 13: (3, 'brl'),
@@ -102,7 +102,7 @@ startexechere( 0 )                                  # start execution here if no
 loadmem()                                           # load binary executable
 ip = 0                                              # start execution at codeseg location 0
 # while instruction is not halt
-while( 1 ):
+while 1:
    ir = getcodemem( ip )                            # - fetch
    ip = ip + 1
    opcode = ir >> opcposition                       # - decode
@@ -111,9 +111,9 @@ while( 1 ):
    addr   = (ir) & addmask
    ic = ic + 1
                                                     # - operand fetch
-   if not (opcodes.has_key( opcode )):
-      tval, treg = trap(0) 
-      if (tval == -1):                              # illegal instruction
+   if opcode not in opcodes:
+      tval, treg = trap(0)
+      if tval == -1:                              # illegal instruction
          break
    memdata = 0                                      #     contents of memory for loads
    if opcodes[ opcode ] [0] == 1:                   #     dec, inc, ret type
@@ -123,22 +123,22 @@ while( 1 ):
       operand2 = getregval( reg2 )
    elif opcodes[ opcode ] [0] == 3:                 #     ld, st, br type
       operand1 = getregval( reg1 )                  #       fetch operands
-      operand2 = addr                     
+      operand2 = addr
    elif opcodes[ opcode ] [0] == 0:                 #     ? type
       break
-   if (opcode == 7):                                # get data memory for loads
-      memdata = getdatamem( operand2 )
+   if opcode == 7:                                # get data memory for loads
+      memdata = getdatamem(operand2)
    # execute
    if opcode == 1:                     # add
       result = (operand1 + operand2) & nummask
       if ( checkres( operand1, operand2, result )):
-         tval, treg = trap(1) 
+         tval, treg = trap(1)
          if (tval == -1):                           # overflow
             break
    elif opcode == 2:                   # sub
       result = (operand1 - operand2) & nummask
       if ( checkres( operand1, operand2, result )):
-         tval, treg = trap(1) 
+         tval, treg = trap(1)
          if (tval == -1):                           # overflow
             break
    elif opcode == 3:                   # dec
@@ -151,7 +151,7 @@ while( 1 ):
       result = operand2
    elif opcode == 12:                  # conditional branch
       result = operand1
-      if result <> 0:
+      if result != 0:
          ip = operand2
    elif opcode == 13:                  # branch and link
       result = ip
@@ -161,21 +161,21 @@ while( 1 ):
    elif opcode == 16:                   # interrupt/sys call
       result = ip
       tval, treg = trap(reg1)
-      if (tval == -1):
+      if tval == -1:
         break
       reg1 = treg
       ip = operand2
    # write back
-   if ( (opcode == 1) | (opcode == 2 ) | 
+   if ( (opcode == 1) | (opcode == 2 ) |
          (opcode == 3) | (opcode == 4 ) ):     # arithmetic
         reg[ reg1 ] = result
-   elif ( (opcode == 7) | (opcode == 9 )):     # loads
+   elif (opcode == 7) | (opcode == 9):     # loads
         reg[ reg1 ] = result
-   elif (opcode == 13):                        # store return address
+   elif opcode == 13:                        # store return address
         reg[ reg1 ] = result
-   elif (opcode == 16):                        # store return address
+   elif opcode == 16:                        # store return address
         reg[ reg1 ] = result
-   # end of instruction loop     
+   # end of instruction loop
 # end of execution
 
-   
+
